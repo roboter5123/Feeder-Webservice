dc0076de076f7e88e5994837d0a60e915120f570
Pascal Gehringer <73991535+roboter5123@users.noreply.github.com>
2023-01-18 17:37:45 +0100

    Update maven.yml

---
+++ target/classes/static/js/dashboard.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+
+    $(".add").click(function () {
+
+        let itemClassToAdd = $(this).closest(".tile").attr("id")
+        let popUpHeadline = $("<h3 id='popUpHeadline'></h3>")
+        popUpHeadline.text("Add "+itemClassToAdd)
+
+        let closePopUp = $("<i class=\"fa-solid fa-x\"></i>")
+        closePopUp.click(function (){
+
+            $('.fullScreen').remove();
+        })
+        let popUpHeading =$("<div id = 'popUpHeading'></div>")
+        popUpHeading.append(popUpHeadline)
+        popUpHeading.append(closePopUp)
+
+
+        let popUpTextField = $("<input type ='text' id = 'identity'>")
+
+        let popUpButton = $("<input type ='button' id = 'popUpSubmit'>")
+        popUpButton.click(() => {
+
+            let parameterValue = $("#identity").val()
+            if (itemClassToAdd === "feeder"){
+
+                fetch("http://localhost:8080/api/feeder?" + new URLSearchParams({"uuid":parameterValue}), {
+                    method: 'POST',
+                    credentials: "include",
+                }).then((response) => {
+
+                    if (response.ok) {
+
+                        location.reload()
+                    }
+                })
+
+            }else if(itemClassToAdd === "schedule"){
+
+                let parameterValue = $("#identity").val()
+                fetch("http://localhost:8080/api/schedule?" + new URLSearchParams({"name":parameterValue}), {
+                    method: 'POST',
+                        credentials: "include",
+                }).then((response) => {
+
+                    if (response.ok) {
+
+                        location.reload()
+                    }
+                })
+
+            }
+        })
+        popUpButton.val("submit")
+
+        let inputLabel = $("<label></label>")
+
+        if (itemClassToAdd === "feeder"){
+
+            inputLabel.text("UUID: ")
+
+        }else if(itemClassToAdd === "schedule"){
+
+            inputLabel.text("Schedule Name:")
+        }
+
+        inputLabel.append(popUpTextField)
+        let popUpInputs = $("<div></div>")
+        popUpInputs.append(inputLabel)
+        popUpInputs.append(popUpButton)
+
+        let popUp = $("<div></div>")
+        popUp.addClass("popUp")
+        popUp.append(popUpHeading)
+        popUp.append(popUpInputs)
+
+        let popUpScreen = $("<div></div>")
+        popUpScreen.addClass("fullScreen")
+        popUpScreen.append(popUp)
+        $("body").append(popUpScreen)
+    })
+})
---
+++ target/classes/static/js/register.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $("#register input[type = 'submit']").click(function (e) {
+
+        e.preventDefault();
+
+        let email = $("#email").val()
+        let password = $("#password").val()
+        let data = {"email": email, "password": password}
+
+        fetch("http://localhost:8080/api/user", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+
+            if (response.ok) {
+
+                window.location.replace("/login")
+
+            } else {
+
+                failedRegister()
+            }
+        })
+    })
+
+    function failedRegister() {
+
+        if ($("#failedRegisterText").length) {
+
+            return
+        }
+        let element = $("<p id ='failedRegisterText'>Failed to register.<br>Please try again.</p>")
+        $("#register").append(element)
+    }
+})
+
