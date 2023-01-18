33d7e0eaf04361df3882ff0d7fafff21e60781be
roboter5123 <pascal.roboter@hotmail.com>
2023-01-18 19:37:32 +0100

    Added JSYPT encryption for datasource password. And changed db password

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:17
 EXPOSE 8080
 ADD /target/feeder-webservice.jar /feeder-webservice.jar
-ENTRYPOINT ["java", "-jar", "/feeder-webservice.jar"]
+ENV encryption_password;
+ENTRYPOINT ["java","-Djasypt.encryptor.password=$encryption_password", "-jar", "/feeder-webservice.jar"]
--- src/main/java/com/roboter5123/feeder/FeederApplication.java
+++ src/main/java/com/roboter5123/feeder/FeederApplication.java
@@ -1,8 +1,10 @@
 package com.roboter5123.feeder;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class FeederApplication {
 
     public static void main(String[] args) {
