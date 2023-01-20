f3424618c92ac85f3547b43ebae6e397fad7757a
roboter5123 <pascal.roboter@hotmail.com>
2023-01-20 15:39:01 +0100

    Fixed delete buttons not working

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
 
             }else if(itemClassToAdd === "schedule"){
 
-                fetch("http://localhost:8080/api/schedule?" + new URLSearchParams({"name":parameterValue}), {
+                fetch("/api/schedule?" + new URLSearchParams({"name":parameterValue}), {
                     method: 'POST',
                         credentials: "include",
                 }).then((response) => {
@@ -87,7 +87,7 @@ $(document).ready(function () {
 
         if (itemType === "feeder") {
 
-            fetch("http://localhost:8080/api/feeder?" + new URLSearchParams({"uuid": itemIdentification}), {
+            fetch("/api/feeder?" + new URLSearchParams({"uuid": itemIdentification}), {
                 method: 'DELETE',
                 credentials: "include",
             }).then((response) => {
@@ -99,7 +99,7 @@ $(document).ready(function () {
             })
         }else if (itemType ==="schedule"){
 
-            fetch("http://localhost:8080/api/schedule?" + new URLSearchParams({"name": itemIdentification}), {
+            fetch("/api/schedule?" + new URLSearchParams({"name": itemIdentification}), {
                 method: 'DELETE',
                 credentials: "include",
             }).then((response) => {
